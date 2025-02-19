4ac3758d30d0689e65f562ad45cdca7e9d29e802
Nicklas Jensen <jensen0710@gmail.com>
2025-02-19 09:45:57 +0100

    add POST to new repair and display info from DB

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -10,4 +10,18 @@ class RepairsController < ApplicationController
   def new
     @repair = Repair.new
   end
+
+  def create
+    @repair = Repair.new(repair_params)
+    if @repair.save
+      redirect_to @repair
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def repair_params
+    params.expect(repair: [ :name, :email, :phone_number, :brand, :error_description, :imei, :serial, :model ])
+  end
 end

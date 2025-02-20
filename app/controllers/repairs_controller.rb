df5e7eb7da895da3ce2328a7ec655ac4c13402a3
Nicklas Jensen <jensen0710@gmail.com>
2025-02-20 14:59:28 +0100

    add search form to navbar

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,6 +1,21 @@
 class RepairsController < ApplicationController
   def index
     @repairs = Repair.all
+
+    # Filter search by id
+    @repairs = @repairs.where(id: params[:query]) if params[:search_in]=="id"
+
+    # Filter search by phone
+    @repairs = @repairs.where(phone_number: params[:query]) if params[:search_in]=="phone_number"
+
+    # Filter search by email
+    @repairs = @repairs.where(email: params[:query]) if params[:search_in]=="email"
+
+    # Filter search by IMEI
+    @repairs = @repairs.where(imei: params[:query]) if params[:search_in]=="imei"
+
+    # Filter search by serial
+    @repairs = @repairs.where(serial: params[:query]) if params[:search_in]=="serial"
   end
 
   def show

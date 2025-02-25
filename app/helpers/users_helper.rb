c5cde80970c2fc1ae62074e7ca4bdeab4845b509
Nicklas Jensen <jensen0710@gmail.com>
2025-02-25 15:36:55 +0100

    improve user role links and remove repetition by moving code to helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def navigation_links(user)
+    links = []
+    links << link_to("Add devices", "") if user.admin?
+    links << link_to("Create users", "") if user.admin? || user.leader?
+    links << link_to("Create parts", "") if user.admin? || user.leader?
+    links << link_to("Change password", "") if user.admin? || user.leader? || user.technician?
+    links.join.html_safe
+  end
 end

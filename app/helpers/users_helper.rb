15a5f44b85c0dd8457437d0b312e0c216f14fef2
Nicklas Jensen <jensen0710@gmail.com>
2025-03-03 15:06:23 +0100

    remove share folder and move content into layouts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def navigation_links(user)
     links = []
-    links << link_to("Account", "") if user.admin? || user.leader? || user.technician?
+    links << link_to("Account", "", class: "active") if user.admin? || user.leader? || user.technician?
     links << link_to("Change password", "") if user.admin? || user.leader? || user.technician?
     links << link_to("Manage users", users_path) if user.admin?
     links << link_to("Manage devices", devices_path) if user.admin?

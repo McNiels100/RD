ddaded57fe16058af358c2a64b70064c9aeb4d5c
Nicklas Jensen <jensen0710@gmail.com>
2025-03-03 10:09:55 +0100

    add user administrator panel that shows links depending on account level

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 module UsersHelper
   def navigation_links(user)
     links = []
-    links << link_to("Add devices", "") if user.admin?
-    links << link_to("Create users", "") if user.admin? || user.leader?
-    links << link_to("Create parts", "") if user.admin? || user.leader?
+    links << link_to("Account", "") if user.admin? || user.leader? || user.technician?
     links << link_to("Change password", "") if user.admin? || user.leader? || user.technician?
+    links << link_to("Manage users", "") if user.admin?
+    links << link_to("Manage devices", "") if user.admin? || user.leader?
+    links << link_to("Excel export", "") if user.admin? || user.leader?
     links.join.html_safe
   end
 end

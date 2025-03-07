035b7668a22316b09c51293974db41f3bbb57976
Nicklas Jensen <jensen0710@gmail.com>
2025-03-07 10:47:47 +0100

    Redirect unauthorized users to panels index instead of root path to show account permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,28 +16,28 @@ class ApplicationController < ActionController::Base
   def require_admin
     unless current_user&.admin?
       flash[:alert] = "You must be an admin to access this section."
-      redirect_to root_path
+      redirect_to panels_index_path
     end
   end
 
   def require_leader
     unless current_user&.leader?
       flash[:alert] = "You must be a leader to access this section."
-      redirect_to root_path
+      redirect_to panels_index_path
     end
   end
 
   def require_technician
     unless current_user&.technician?
       flash[:alert] = "You must be a technician to access this section."
-      redirect_to root_path
+      redirect_to panels_index_path
     end
   end
 
   def require_admin_or_leader
     unless current_user&.admin? || current_user&.leader?
       flash[:alert] = "You must be an admin or a leader to access this section."
-      redirect_to root_path
+      redirect_to panels_index_path
     end
   end
 end

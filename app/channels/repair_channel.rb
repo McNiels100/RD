68a10d18a660126dd4356172b270d1dc39992a1b
Nicklas Jensen <jensen0710@gmail.com>
2025-03-11 21:30:09 +0100

    make RepairChannel lock on subscribe and unlock on unsubscribe

--- app/channels/repair_channel.rb
+++ app/channels/repair_channel.rb
@@ -1,9 +1,16 @@
 class RepairChannel < ApplicationCable::Channel
   def subscribed
+    # Find the repair and lock it for the current user
+    repair = Repair.find(params[:id])
+    repair.lock!(current_user.email_address) if repair && current_user
+
+    # Stream updates for this repair
     stream_from "repair_#{params[:id]}"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    # Find the repair and unlock it when the user unsubscribes
+    repair = Repair.find(params[:id])
+    repair.unlock! if repair && repair.locked_by?(current_user.email_address)
   end
 end

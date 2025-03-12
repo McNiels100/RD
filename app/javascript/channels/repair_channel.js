8360c1218d588f667181d5d4608740aadb4317a1
Nicklas Jensen <jensen0710@gmail.com>
2025-03-12 12:18:35 +0100

    fix js bug where dom_id is not available

--- app/javascript/channels/repair_channel.js
+++ app/javascript/channels/repair_channel.js
@@ -1,21 +1,30 @@
 import consumer from "channels/consumer"
 
-const repairContainer = document.getElementById('repair_container');
-const repairId = repairContainer.dataset.repairId;
+document.addEventListener('turbo:load', () => { // Listen for turbo:load event
+  console.log('turbo:load event fired');
+  const repairContainer = document.getElementById('repair_container');
 
-if (repairId) {
-  consumer.subscriptions.create({ channel: "RepairChannel", id: repairId }, {
-    connected() {
-      console.log('connected to repair channel');
-    },
-    disconnected() {
-      console.log('disconnected from repair channel');
-    },
-    received(data) {
-      console.log('received data from repair channel');
-      document.getElementById(`repair_${repairId}`).innerHTML = data.html;
+  if (repairContainer) {
+    const repairId = repairContainer.dataset.repairId;
+    if (repairId) {
+      consumer.subscriptions.create({ channel: "RepairChannel", id: repairId }, {
+        connected() {
+          console.log('connected to repair channel');
+        },
+        disconnected() {
+          console.log('disconnected from repair channel');
+        },
+        received(data) {
+          console.log('received data from repair channel');
+          document.getElementById(`repair_${repairId}`).innerHTML = data.html;
+        }
+      });
+    } else {
+      console.error("Could not find repair ID.");
     }
-  });
-} else {
-  console.error("Could not find repair ID.");
-}
+  }
+});
+
+
+
+

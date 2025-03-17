1af09969274c38e8c9a0388ddb4fcf9941baea2d
Nicklas Jensen <jensen0710@gmail.com>
2025-03-17 13:24:33 +0100

    fix typo in devices.yml and tests for repairs_controller and pannels_controller removed

--- test/controllers/panels_controller_test.rb
+++ test/controllers/panels_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class PanelsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get panels_index_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end

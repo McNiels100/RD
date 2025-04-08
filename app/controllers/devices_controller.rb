3d3e6fcb06b03b286c3a22b0ba50425c7d4ab210
Nicklas Jensen <jensen0710@gmail.com>
2025-04-08 15:18:42 +0200

    Add edit and update to devices_controller. Move form to a partial. Rendered in new and edit views. Delete show view.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -12,9 +12,24 @@ class DevicesController < ApplicationController
     @device = Device.find(params[:id])
   end
 
+  def edit
+    @device = Device.find(params[:id])
+  end
+
+  def update
+    @device = Device.find(params[:id])
+    if @device.update(device_params)
+      flash[:success] = "Device updated successfully!"
+      redirect_to devices_path
+    else
+      flash.now[:error] = @device.errors.full_messages.to_sentence + "!"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   def create
-    @device = Device.new(repair_params)
+    @device = Device.new(device_params)
     if @device.save
       redirect_to devices_path
     else
@@ -30,7 +45,7 @@ class DevicesController < ApplicationController
   end
 
   private
-  def repair_params
-    params.expect(device: [ :brand, :device_type, :tat_neutral, :tat_unsatisfied, :tat_very_unsatisfied ])
+  def device_params
+    params.require(:device).permit(:brand, :device_type, :tat_neutral, :tat_unsatisfied, :tat_very_unsatisfied)
   end
 end

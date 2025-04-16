408cf84c9afa895c73fa40fa64219dcd1fc88838
Nicklas Jensen <jensen0710@gmail.com>
2025-04-16 14:27:13 +0200

    Edit tat_helpers to use DRY

--- app/helpers/repairs_helper.rb
+++ app/helpers/repairs_helper.rb
@@ -1,42 +1,47 @@
 module RepairsHelper
-  def calculate_tat_stage(device, created_at, output)
-    tat_days = working_days_between(created_at, Time.zone.now)
+  def calculate_tat_info(device, created_at, output = nil)
+      tat_days = working_days_between(created_at, Time.zone.now)
 
-    case tat_days
-    when 0..device.tat_neutral
-      @tat_status = "Good"
-      @days_to_next_stage = device.tat_neutral - tat_days
-      tat_css = output + "-green"
-    when device.tat_neutral..device.tat_unsatisfied
-      @tat_status = "Neutral"
-      @days_to_next_stage = device.tat_unsatisfied - tat_days
-      tat_css = output + "-yellow"
-    when device.tat_unsatisfied..device.tat_very_unsatisfied
-      @tat_status = "Unsatisfied"
-      @days_to_next_stage = device.tat_very_unsatisfied - tat_days
-      tat_css = output + "-orange"
-    else
-      @tat_status = "Very unsatisfied"
-      @days_to_next_stage = "N/A"
-      tat_css = output + "-red"
+      case tat_days
+      when 0..device.tat_neutral
+        status = "Good"
+        days_to_next_stage = device.tat_neutral - tat_days
+        css_class = output ? "#{output}-green" : "green"
+      when device.tat_neutral..device.tat_unsatisfied
+        status = "Neutral"
+        days_to_next_stage = device.tat_unsatisfied - tat_days
+        css_class = output ? "#{output}-yellow" : "yellow"
+      when device.tat_unsatisfied..device.tat_very_unsatisfied
+        status = "Unsatisfied"
+        days_to_next_stage = device.tat_very_unsatisfied - tat_days
+        css_class = output ? "#{output}-orange" : "orange"
+      else
+        status = "Very unsatisfied"
+        days_to_next_stage = "N/A"
+        css_class = output ? "#{output}-red" : "red"
+      end
+
+      {
+        status: status,
+        days_to_next_stage: days_to_next_stage,
+        css_class: css_class
+      }
     end
-    tat_css
-  end
 
-  def determine_tat_status(device, created_at)
-    tat_days = working_days_between(created_at, Time.zone.now)
-
-    case tat_days
-    when 0..device.tat_neutral
-      "Good"
-    when device.tat_neutral..device.tat_unsatisfied
-      "Neutral"
-    when device.tat_unsatisfied..device.tat_very_unsatisfied
-      "Unsatisfied"
-    else
-      "Very unsatisfied"
+    # Return the CSS class for styling (replacing calculate_tat_stage)
+    def set_tat_css(device, created_at, output)
+      calculate_tat_info(device, created_at, output)[:css_class]
     end
-  end
+
+    # Return just the status string for filtering
+    def determine_tat_status(device, created_at)
+      calculate_tat_info(device, created_at)[:status]
+    end
+
+    # Return just the days to next stage
+    def days_to_next_tat_stage(device, created_at)
+      calculate_tat_info(device, created_at)[:days_to_next_stage]
+    end
 
   def find_tat(repair)
     device = Device.find_by(brand: repair.brand, device_type: repair.device_type)

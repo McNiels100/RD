cedd0985b166d6ac857322fd45da15ec2b67f00f
Nicklas Jensen <jensen0710@gmail.com>
2025-04-25 14:57:14 +0200

    Update toggle_controller.js to make sure turbo frames are reloaed on toggle if any.

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,15 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  // Define the target element that will be toggled
   static targets = ["toggleable"];
-  // Define optional CSS classes to toggle
   static classes = ["hidden"];
 
   connect() {}
 
   toggle() {
-    // Toggles the class defined in static classes
+    // Toggle visibility
     this.toggleableTarget.classList.toggle(this.hiddenClass);
+
+    // If we're showing the modal, reload the inventory frame if any
+    if (!this.toggleableTarget.classList.contains(this.hiddenClass)) {
+      // Reset the inventory_select frame
+      const frame = document.getElementById("inventory_select");
+      if (frame) frame.src = frame.src;
+    }
   }
 }

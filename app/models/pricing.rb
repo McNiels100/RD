94633ba11960c851d481cc8ef17979573a26e0cd
Nicklas Jensen <jensen0710@gmail.com>
2025-05-19 16:17:30 +0200

    Fix bug where pricing is applied even if the effective date is not valid

--- app/models/pricing.rb
+++ app/models/pricing.rb
@@ -4,10 +4,11 @@ class Pricing < ApplicationRecord
   validate :no_whitespace_in_part_number
 
   def self.current_price_for(part_number)
-    where(part_number: part_number)
-      .order(effective_date: :desc)
-      .first
-  end
+      where(part_number: part_number)
+        .where("effective_date <= ?", Date.today)
+        .order(effective_date: :desc)
+        .first
+    end
 
   private
   def no_whitespace_in_part_number
